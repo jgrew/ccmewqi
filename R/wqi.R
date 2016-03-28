2e60a5d0e3c39a33e630df9890432cbf66c61332
jgrew <justin.grewal@ocpw.ocgov.com>
2016-03-28 11:23:55 -0700

    Extra wqi method

--- R/wqi.R
+++ R/wqi.R
@@ -5,17 +5,22 @@
 #' @param F1
 #' @param F2
 #' @param F3
+#' @param method optional CCAMP method.
 #' 
 #' @export
 #' @details
 #' Inputs must be numeric.
 #' 
 #' @seealso \url{http://www.ccme.ca/en/resources/canadian_environmental_quality_guidelines/calculators.html}
-wqi <- function(F1, F2, F3) {
+wqi <- function(F1, F2, F3, method = NULL) {
  
    is.numeric(F1) || stop('F1 is non-numeric')
    is.numeric(F2) || stop('F2 is non-numeric')
    is.numeric(F3) || stop('F3 is non-numeric')
    
-   100 - (sqrt(F1 ^ 2 + F2 ^ 2 + F3 ^ 2) / 1.732)
+   if(is.null(method)){
+     100 - (sqrt(F1 ^ 2 + F2 ^ 2 + F3 ^ 2) / 1.732)
+   } else {
+     100 - (sqrt(F2 ^ 2 + F3 ^ 2) / 1.414)
+   }
 }
